38440122e2e21f16fb87e31e94b7deae21d7d76e
Clément Nussbaumer <clement@astutus.org>
2022-12-12 21:38:48 +0100

    success(2022.12.07): part2 !

---
+++ 2022/07/part2/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Directory struct {
+	parent  *Directory
+	subdirs map[string]*Directory
+	files   map[string]int
+	path    string
+	size    int
+}
+
+func main() {
+
+	log.Println("AOC - 2022.12.07+2")
+
+	inputBytes, err := os.ReadFile("../input")
+	if err != nil {
+		log.Fatalf("couldn't open input file. error %v", err)
+		return
+	}
+
+	var rootDir = &Directory{
+		parent:  nil,
+		subdirs: make(map[string]*Directory),
+		path:    "/",
+		files:   make(map[string]int),
+	}
+	var currentDir = rootDir
+
+	input := strings.Split(string(inputBytes), "\n")
+	for _, line := range input[1:] {
+		spl := strings.Split(line, " ")
+		if line[0] == '$' {
+			if spl[1] == "cd" && spl[2] == ".." {
+				currentDir.parent.size += currentDir.size
+				currentDir = currentDir.parent
+			} else if spl[1] == "cd" {
+				cd := spl[2]
+				newDir := Directory{
+					parent:  currentDir,
+					path:    currentDir.path + cd + "/",
+					files:   make(map[string]int),
+					subdirs: make(map[string]*Directory),
+				}
+				if currentDir.subdirs != nil {
+					currentDir.subdirs[cd] = &newDir
+				}
+				currentDir = &newDir
+			} else if spl[1] == "ls" {
+				continue
+			}
+		} else if spl[0] == "dir" {
+			continue
+		} else {
+			size, _ := strconv.Atoi(spl[0])
+			name := spl[1]
+			currentDir.files[name] = size
+			currentDir.size += size
+		}
+	}
+
+	for currentDir != rootDir {
+		currentDir.parent.size += currentDir.size
+		currentDir = currentDir.parent
+	}
+
+	threshold := 30*1e6 - (70*1e6 - rootDir.size)
+	fmt.Println("threshold:\t", threshold)
+
+	fmt.Println("smallest:\t\t", rootDir.smallestDirBiggerThanThreshold(threshold))
+	fmt.Println("sum:\t\t", rootDir.sumLess100000())
+}
+
+func (d *Directory) smallestDirBiggerThanThreshold(threshold int) (sum int) {
+	var size int = 1e10
+	for _, dir := range d.subdirs {
+		ret := dir.smallestDirBiggerThanThreshold(threshold)
+		if ret < size {
+			size = ret
+		}
+	}
+	if d.size >= threshold && d.size < size {
+		size = d.size
+	}
+
+	return size
+}
+
+func (d *Directory) sumLess100000() (sum int) {
+	for _, dir := range d.subdirs {
+		sum += dir.sumLess100000()
+	}
+	if d.size < 1e5 {
+		sum += d.size
+	}
+
+	return sum
+}
