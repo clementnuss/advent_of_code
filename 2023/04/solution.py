3841db51f6bf5ea7e82bdfd81879b9941fe512ff
Clément Nussbaumer <clement.nussbaumer@postfinance.ch>
2023-12-04 22:07:52 +0100

    success(2023.12.04): part2 :)
    Signed-off-by: Clément Nussbaumer <clement.nussbaumer@postfinance.ch>

--- 2023/04/solution.py
+++ 2023/04/solution.py
@@ -7,17 +7,22 @@ with open("04/input") as f:
     lines = f.read().strip().split("\n")
 
 res1, res2 = 0, 0
+copies = [1] * len(lines)
 
-for l in lines:
+for i, l in enumerate(lines):
     spl = l.split(":")[1].split("| ")
-    power = -1
+    count = 0
     winning = {match.group(0) for match in re.finditer(r"\d+", spl[0])}
     # print(winning, type(winning))
     for match in re.finditer(r"\d+", spl[1]):
-        print(match.group(0))
         if match.group(0) in winning:
-            power += 1
-    if power >= 0:
-        res1 += 1 << power
+            count += 1
+    if count > 0:
+        res1 += 1 << count - 1
+        for j in range(i+1,i+count+1):
+            copies[j] += copies[i]
+
+
 
 print(f"res1: {res1}")
+print(f"res2: {sum(copies)}")
