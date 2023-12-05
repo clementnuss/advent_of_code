4efc484f7a2f462b88819618afec0fc27512a1da
Clément Nussbaumer <clement.nussbaumer@postfinance.ch>
2023-12-05 20:27:19 +0100

    success(2023.12.05): part2 ! 7m18s, can be optimized 😅
    Signed-off-by: Clément Nussbaumer <clement.nussbaumer@postfinance.ch>

--- 2023/05/solution.py
+++ 2023/05/solution.py
@@ -1,41 +1,51 @@
+import itertools
 import math
 import re
 from collections import defaultdict
 
-# with open("05/input") as f:
-with open("05/example_input") as f:
+with open("05/input") as f:
+# with open("05/example_input") as f:
     parts = f.read().strip().split("\n\n")
 
 seeds = list(map(int, re.findall(r"\d+", parts[0])))
 seedstuple = list(zip(seeds[::2], seeds[1::2]))
-seeds2range = [range(start, ln) for start, ln in seedstuple]
+
+fns = []
+finvs = []
 
 for paragraph in parts[1:]:
     lines = paragraph.split("\n")
-    # currMapStr = lines[0].split(" ")[0]
     maps: list[(int, int, int)] = []
-    for l in lines[1:]:
-        m = list(map(int, re.findall(r"\d+", l)))
-        dst, src, ln = m[0], m[1], m[2]
-        maps.append((src, dst, ln))
+    nbrs = [list(map(int, re.findall(r"\d+", l))) for l in lines[1:]]
 
-    newSeeds = []
-    for x in seeds:
-        mapped = False
-        for src, dst, ln in maps:
-            if x >= src and x < src + ln:
-                newSeeds.append(dst + (x - src))
-                mapped = True
-                break
+    def f(x, nbrs=nbrs):
+        for dst, src, n in nbrs:
+            if src <= x and x < src + n:
+                return x - src + dst
+        return x
 
-        if not mapped:
-            newSeeds.append(x)
-    # newSeeds = list(map(lambda x: currMap.get(x, x), seeds))
-    seeds = newSeeds
+    def finv(x, nbrs=nbrs):
+        for src, dst, n in nbrs:
+            if src <= x and x < src + n:
+                return x - src + dst
+        return x
+
+    fns.append(f)
+    finvs.append(finv)
 
 
-res1, res2 = 0, 0
-res1 = min(seeds)
+def F(x, functions):
+    for f in functions:
+        x = f(x)
+    return x
 
+
+res1 = min(map(lambda x: F(x, fns), seeds))
 print(f"res1: {res1}")
-print(f"res2: {res2}")
+
+for i in itertools.count():
+    src = F(i, finvs[::-1])
+    for start, length in seedstuple:
+        if src >= start and src < start + length:
+            print(f"res2: {i}")
+            exit(0)
