0cefab8d6b9d9c5996aaedc4322861cfa254faec
Clément Nussbaumer <clement.nussbaumer@postfinance.ch>
2023-12-18 18:34:19 +0100

    chore(2023.12.17): added heuristic function - minimal benefit
    and also not always correct. when the heuristic has too much weight,
    incorrect solutions are produced
    
    Signed-off-by: Clément Nussbaumer <clement.nussbaumer@postfinance.ch>

--- 2023/17/solution.py
+++ 2023/17/solution.py
@@ -18,22 +18,28 @@ with open("./17/input") as f:
 
 grid = {i + 1j * j: int(x) for j, l in enumerate(lines) for i, x in enumerate(l)}
 ROWS, COLS = len(lines), len(lines[0])
+TARGET = (COLS - 1) + (ROWS - 1) * 1j
 INVALID = 10000000
 
 
+def heuristic(z1: complex):
+    z = z1 - TARGET
+    return (z.real**2 + z.imag**2) ** 0.5
+
+
 def a_star(p2=False):
-    heap = [(0, -1, 0 + 0j, 1 + 0j, 3 if not p2 else 10)]
+    heap = [(heuristic(0 + 0j), 0, -1, 0 + 0j, 1 + 0j, 3 if not p2 else 10)]
     best = defaultdict(lambda: 1_000_000_000_000)  # z, dir, remaining
 
     step = 0
     while heap:
-        heat_loss, _, z, dir, rem = heapq.heappop(heap)
+        h, heat_loss, _, z, dir, rem = heapq.heappop(heap)
         if z not in grid:
             continue
 
-        if z == (COLS - 1) + (ROWS - 1) * 1j:
+        if z == TARGET:
             if not p2 or rem < 7:
-                # print(step)
+                print(h, step)
                 return heat_loss
 
         newdirs = []  # (new dir, remaining moves)
@@ -56,7 +62,7 @@ def a_star(p2=False):
                 continue
 
             best[z2, d, rem] = dist2
-            heapq.heappush(heap, (dist2, step, z2, d, rem))
+            heapq.heappush(heap, (dist2 + 2 * heuristic(z2), dist2, step, z2, d, rem))
             step += 1
 
 
